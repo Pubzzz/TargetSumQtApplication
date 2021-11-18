fab9628671980051c027b7ba268b9b0fb7a75d18
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-11-18 14:30:05 +0530

    IntermediateWindow

---
+++ intermediatewindow.cpp
@@ -0,0 +1,6 @@
+#include "intermediatewindow.h"
+
+IntermediateWindow::IntermediateWindow()
+{
+
+}
---
+++ intermediatewindow.h
@@ -0,0 +1,12 @@
+#ifndef INTERMEDIATEWINDOW_H
+#define INTERMEDIATEWINDOW_H
+
+#include <QMainWindow>
+
+class IntermediateWindow
+{
+public:
+    IntermediateWindow();
+};
+
+#endif // INTERMEDIATEWINDOW_H
